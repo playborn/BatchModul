291cb84cf7e862da31f3acf69cea3477bf4a328c
playborn <93843957+playborn@users.noreply.github.com>
2022-04-16 16:24:44 +0200

    Add files via upload

--- modul/file/isFile.bat
+++ modul/file/isFile.bat
@@ -1,3 +1,8 @@
+:: v0.0.2
+::--changeLog--
+::	+double check expected Attributes.
+::  +BUG FIX missing :usage 
+
 :isFile "File"
 set "return="
 setlocal
@@ -5,12 +10,24 @@ if "%~1"=="" goto usage
 if not exist "%~1" exit /B 2
 set "isFile="
 set "fileAttributes=%~a1"
-if /i "%fileAttributes:~0,1%"=="d" (
-	set "isFile=false"
-) else (
+
+if "%fileAttributes:~0,1%"=="-" (
 	set "isFile=true"
+) else (
+	if /i "%fileAttributes:~0,1%"=="d" (
+		set "isFile=false"
+	) else (
+		exit /b 10
+	)
 )
+
 endlocal & (
 	set "return=%isFile%"
 )
 exit /B 0
+
+
+
+:usage
+echo HELP TEXT
+exit /b 1
